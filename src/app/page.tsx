e649d3f543a0c8683ceee5793d8d4c1ccea395f3
Alessandro Chinchilla <ale030401@gmail.com>
2024-07-12 20:58:48 -0600

    Add: main page content and form container + styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
 import Image from "next/image";
+import { Fragment } from "react";
+import FormContainer from "./components/FormContainer";
 
 export default function Home() {
-	return <main>Hello World</main>;
+	return (
+		<Fragment>
+			<header className="px-4 py-3 flex ">
+				<Image
+					src={"/logo.png"}
+					alt="Logo"
+					width={162.59}
+					height={40}
+				/>
+				<img src="/line.svg" alt="" className="mx-8" />
+			</header>
+			<main className="font-albert_sans flex flex-col  content-center items-center min-h-screen ">
+				<div className="mt-10">
+					<div className=" mb-8">
+						<h3 className="font-bold text-bold-blue self-start text-2xl">
+							Crea una orden
+						</h3>
+						<p className="text-regular-blue text-s">
+							Dale una ventaja competitiva a tu negocio con
+							entregas
+							<b> el mismo día</b> (Área Metropolitana) y
+							<b> el día siguiente</b> a nivel nacional.
+						</p>
+					</div>
+
+					<FormContainer />
+				</div>
+			</main>
+		</Fragment>
+	);
 }
